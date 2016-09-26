6e7776a1ac5af73d2f2e22a408dcddd369f01af1
Andrew Ross <andrew@vermonster.com>
2016-09-26 09:56:56 -0400

    refactor showing line numbers

--- c_utest.h
+++ c_utest.h
@@ -10,7 +10,7 @@
 
 static char _test_names[MAX_TEST_COUNT][MAX_NAME_CHARS];
 static char _failure_msgs[MAX_TEST_COUNT][MAX_MSG_CHARS];
-static int _fail_line_nos[MAX_TEST_COUNT];
+static int _failed_lines[MAX_TEST_COUNT];
 typedef void (*_UnitTest)(void);
 static _UnitTest _test_funcs[MAX_TEST_COUNT];
 static int _failed_tests[MAX_TEST_COUNT];
@@ -18,7 +18,6 @@ static int _test_count = 0;
 static int _fail_count;
 static int _test_status;
 static int _current_test;
-static int _line_number;
 
 #define RUN_TESTS() do {\
   printf("About to run %d tests...\n\n", _test_count);\
@@ -26,7 +25,6 @@ static int _line_number;
   _current_test = 0;\
   while (_current_test < _test_count) {\
     _test_status = 0;\
-    _line_number = 0;\
     _test_funcs[_current_test]();\
     if (_test_status) printf("%sF%s", KRED, KNRM);\
     else printf("%s.%s", KGRN, KNRM);\
@@ -35,7 +33,7 @@ static int _line_number;
   printf("\n");\
   for (int i = 0; i < _fail_count; i++)\
     printf("\n%s%s%s failed at line %d with:\n\t%s\n",\
-        KYEL, _test_names[_failed_tests[i]], KNRM, _fail_line_nos[i], _failure_msgs[i]);\
+        KYEL, _test_names[_failed_tests[i]], KNRM, _failed_lines[i], _failure_msgs[i]);\
   if (_fail_count) printf("\nTotal failures: %d\n\n", _fail_count);\
   else printf("\nAll tests passed!\n\n");\
   return _fail_count;\
@@ -44,7 +42,7 @@ static int _line_number;
 #define FAIL_WITH(...) do {\
   _test_status = 1;\
   _failed_tests[_fail_count] = _current_test;\
-  _fail_line_nos[_fail_count] = _line_number;\
+  _failed_lines[_fail_count] = __LINE__;\
   char _failure_msg[MAX_MSG_CHARS];\
   sprintf(_failure_msg, ##__VA_ARGS__);\
   strcpy(_failure_msgs[_fail_count], _failure_msg);\
@@ -52,26 +50,16 @@ static int _line_number;
   return;\
 } while(0)
 
-#define ASSERT(a) do { _line_number = __LINE__; \
-  if (!a) FAIL_WITH("assertion error"); } while(0)
+#define ASSERT(a) if (!a) FAIL_WITH("assertion error");
 
-#define ASSERT_EQUAL_INTS(a, b) do { _line_number = __LINE__; \
-  if (a != b) FAIL_WITH("expected %d to equal %d", a, b); } while(0)
+#define ASSERT_EQUAL_INTS(a, b) if (a != b) FAIL_WITH("expected %d to equal %d", a, b);
+#define ASSERT_DIFFN_INTS(a, b) if (a == b) FAIL_WITH("expected %d NOT to equal %d", a, b);
 
-#define ASSERT_DIFFN_INTS(a, b) do { _line_number = __LINE__; \
-  if (a == b) FAIL_WITH("expected %d NOT to equal %d", a, b); } while(0)
+#define ASSERT_EQUAL_PTRS(a, b) if (a != b) FAIL_WITH("expected %p to equal %p", a, b);
+#define ASSERT_DIFFN_PTRS(a, b) if (a == b) FAIL_WITH("expected %p NOT to equal %p", a, b);
 
-#define ASSERT_EQUAL_PTRS(a, b) do { _line_number = __LINE__; \
-  if (a != b) FAIL_WITH("expected %p to equal %p", a, b); } while(0)
-
-#define ASSERT_DIFFN_PTRS(a, b) do { _line_number = __LINE__; \
-  if (a == b) FAIL_WITH("expected %p NOT to equal %p", a, b); } while(0)
-
-#define ASSERT_EQUAL_STRS(a, b) do { _line_number = __LINE__; \
-  if (strcmp(a, b)) FAIL_WITH("expected %s to equal %s", a, b); } while(0)
-
-#define ASSERT_DIFFN_STRS(a, b) do { _line_number = __LINE__; \
-  if (!strcmp(a, b)) FAIL_WITH("expected %s NOT to equal %s", a, b); } while(0)
+#define ASSERT_EQUAL_STRS(a, b) if ( strcmp(a, b)) FAIL_WITH("expected %s to equal %s", a, b);
+#define ASSERT_DIFFN_STRS(a, b) if (!strcmp(a, b)) FAIL_WITH("expected %s NOT to equal %s", a, b);
 
 #define TEST_THAT(name) do {\
   _test_funcs[_test_count] = name;\
